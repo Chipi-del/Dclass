88ba3253587f6901349d37073dc4e6bb4218ff51
Chipi-del <56266144+Chipi-del@users.noreply.github.com>
2021-11-07 23:20:32 +0300

    Add files via upload

---
+++ CForm/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace CForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+            KeyPreview = true;
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.Enter) Рассчёт(); };
+          
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+        private void Рассчёт()
+        {
+
+        n: string расстояние_до_цели = Interaction.InputBox("Введите расстояние до цели", " ");
+            if (расстояние_до_цели == "")
+            { goto n; }
+          n1: string скорость_полёта_снаряда = Interaction.InputBox("Введите скорость полёта снаряда", " ");
+            if (скорость_полёта_снаряда == "")
+            { goto n1; }
+            
+            ;
+            double ugol = Math.Sin(double.Parse(расстояние_до_цели)*9.8 / Math.Pow(double.Parse(скорость_полёта_снаряда),2));
+            MessageBox.Show(Convert.ToString(ugol));
+
+        }
+    }
+}
